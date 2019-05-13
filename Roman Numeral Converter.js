32cce58aef59c377d053997f6c635fc7489cf6dc
Nick Haras <nick@macexperts.gr>
2019-05-13 11:21:10 +0300

    Added Roman numeral converter

---
+++ Roman Numeral Converter.js
@@ -0,0 +1,25 @@
+const convertToRoman = num => {
+  let map = {
+      M: 1000,
+      CM: 900,
+      D: 500,
+      CD: 400,
+      C: 100,
+      XC: 90,
+      L: 50,
+      XL: 40,
+      X: 10,
+      IX: 9,
+      V: 5,
+      IV: 4,
+      I: 1
+    },
+    res = "";
+  for (let i in map) {
+    while (num >= map[i]) {
+      res += i;
+      num -= map[i];
+    }
+  }
+  return res;
+};
